45e0daff1afd9dbd33fce8accecb25a9d6b41a14
Keito <93522137+keito0612@users.noreply.github.com>
2022-11-26 15:20:43 +0900

    InteractiveViewerからPhotoViewに変更

--- lib/view/photo.dart
+++ lib/view/photo.dart
@@ -1,6 +1,7 @@
 import 'dart:convert';
 
 import 'package:flutter/material.dart';
+import 'package:photo_view/photo_view.dart';
 
 class Photo extends StatelessWidget {
   Photo({this.medicineImage});
@@ -8,31 +9,22 @@ class Photo extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        backgroundColor: Colors.pink[100],
-        appBar: AppBar(
-          title: const Text('写真'),
-          backgroundColor: Colors.pink[100],
-          leading: IconButton(
-            onPressed: () {
-              Navigator.pop(context);
-            },
-            icon: const Icon(Icons.arrow_back_ios),
-          ),
-        ),
-        body: SingleChildScrollView(
-          child: Column(children: [
-            medicineImage != null
-                ? Center(
-                    child: InteractiveViewer(
-                        child: Image.memory(base64Decode(medicineImage!),
-                            fit: BoxFit.cover)),
-                  )
-                : showIndicator(context)
-          ]),
-        ),
-      ),
-    );
+        home: Scaffold(
+            backgroundColor: Colors.pink[100],
+            appBar: AppBar(
+              title: const Text('写真'),
+              backgroundColor: Colors.pink[100],
+              leading: IconButton(
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+                icon: const Icon(Icons.arrow_back_ios),
+              ),
+            ),
+            body: Container(
+              child: PhotoView(
+                  imageProvider: MemoryImage(base64Decode(medicineImage!))),
+            )));
   }
 
   Widget showIndicator(BuildContext context) {
